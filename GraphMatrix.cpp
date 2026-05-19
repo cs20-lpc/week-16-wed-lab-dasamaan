ea23080b4546fa7a5558587d8ca3014ef929b2a0
dasamaan <dasamaan@zonemail.clpccd.edu>
2026-05-19 14:00:41 -0700

    done

--- GraphMatrix.cpp
+++ GraphMatrix.cpp
@@ -1,15 +1,46 @@
 #include "GraphMatrix.hpp"
+#include <string>
 
-GraphMatrix::GraphMatrix(int numV, GType type) {
-    // TODO
+using namespace std;
+
+GraphMatrix::GraphMatrix(int numV, GType type)
+: Graph(numV, 0, type) {
+
+    cout << "Constructor called with v = " << numV << endl;
+    cout << "Stored v = " << v << endl;
+
+    // Allocate rows
+    adjMatrix = new bool*[v];
+
+    // Allocate columns
+    for (int i = 0; i < v; i++) {
+        adjMatrix[i] = new bool[v];
+    }
+
+    // Initialize matrix to false
+    for (int i = 0; i < v; i++) {
+        for (int j = 0; j < v; j++) {
+            adjMatrix[i][j] = false;
+        }
+    }
 }
 
 GraphMatrix::~GraphMatrix() {
-    // TODO
+    for (int i = 0; i < v; i++) {
+        delete[] adjMatrix[i];
+    }
+
+    delete[] adjMatrix;
+
 }
 
 bool GraphMatrix::isEdge(int i, int j) const {
-    // TODO
+    if (i < 0 || i >= v || j < 0 || j >= v) {
+        throw std::out_of_range("toggleEdge: invalid vertex");
+    }
+
+    return adjMatrix[i][j];
+
 }
 
 ostream& GraphMatrix::print(ostream& os) const {
@@ -37,5 +68,23 @@ ostream& GraphMatrix::print(ostream& os) const {
 }
 
 void GraphMatrix::toggleEdge(int i, int j) {
-    // TODO
+    if (i < 0 || i >= v || j < 0 || j >= v) {
+        throw std::out_of_range("toggleEdge: invalid vertex");
+    }
+
+    // Toggle edge
+    adjMatrix[i][j] = !adjMatrix[i][j];
+
+    // Update edge count
+    if (adjMatrix[i][j]) {
+        e++;
+    }
+    else {
+        e--;
+    }
+
+    // If undirected, mirror edge
+    if (graphType == UNDIRECTED && i != j) {
+        adjMatrix[j][i] = adjMatrix[i][j];
+    }
 }
